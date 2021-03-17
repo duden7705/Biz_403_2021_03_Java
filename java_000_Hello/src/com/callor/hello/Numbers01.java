8c5fefbfe0394f4c1f042bb11a6145abb8282097
duden7705 <duden7705@naver.com>
2021-03-17 10:04:06 +0900

    연산복습

---
+++ java_000_Hello/src/com/callor/hello/Numbers01.java
@@ -0,0 +1,15 @@
+package com.callor.hello;
+
+public class Numbers01 {
+	public static void main(String[] args) {
+		
+		System.out.println("25 + 2 =" + (25 + 2));
+		
+		
+		System.out.print("25 + 2 = ");
+		System.out.println(25 + 2);
+		System.out.print("30 + 10 = ");
+		System.out.println(30 + 10);
+		
+	}
+}
