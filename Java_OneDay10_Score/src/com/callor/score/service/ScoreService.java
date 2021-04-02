4e0d203aa1546ced0170bbd968ac79cf75a55202
duden7705 <duden7705@naver.com>
2021-04-02 09:23:53 +0900

    OneDay

---
+++ Java_OneDay10_Score/src/com/callor/score/service/ScoreService.java
@@ -0,0 +1,20 @@
+package com.callor.score.service;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class ScoreService {
+	
+	public void MenuScore() {
+		
+		Scanner scan;
+		List<Scanner> scoreList;
+		
+		while(true) {
+			System.out.println("=".repeat(50));
+			System.out.println("빛고을 고등학교 성적처리 프로젝트 2021");
+			System.out.println("=".repeat(50));
+		}
+	}
+
+}
