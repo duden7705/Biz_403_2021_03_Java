aa39ae18cf5120605638c988dc35d7736fc3f6a1
duden7705 <duden7705@naver.com>
2021-04-26 16:26:51 +0900

    0426

---
+++ Java_023_Finale/src/com/callor/fine/addInt.java
@@ -0,0 +1,10 @@
+package com.callor.fine;
+
+public class addInt {
+
+	public int add(int num1, int num2) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ Java_023_Finale/src/com/callor/fine/service/ScoreServiceImplV1.java
@@ -0,0 +1,17 @@
+package com.callor.fine.service;
+
+public class ScoreServiceImplV1 implements ScoreService {
+
+	@Override
+	public void inputScore() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void printList() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
